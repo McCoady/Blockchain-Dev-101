595a171f89730c01174a6619ab0dc5d790c8d280
McCoady <78250146+McCoady@users.noreply.github.com>
2021-06-19 23:57:53 +0900

    Initial token draft
    Coin created, 0 decimals, deployer has minter ability.
    
    Prerequisites:
    npm @openzeppelin/contract installation required
    
    Issues:
    Token name/symbol still undecided
    
    Truffle testing of contract not done (but no issues encountered manual testing on Truffle Console/Ganache)

---
+++ contracts/1_Discoin.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Discoin is ERC20, Ownable {
+    constructor() ERC20("Discoin", "DSC") {}
+
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
