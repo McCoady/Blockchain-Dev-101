5c89afadd52040f3cc486a334b4665801630e4b8
McCoady <78250146+McCoady@users.noreply.github.com>
2021-07-15 21:50:43 +0900

    Updated App layout

--- interface/src/Navbar.js
+++ interface/src/Navbar.js
@@ -4,20 +4,20 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" id="Nav">
             <div className="container-fluid">
-                <a className="navbar-brand mb-0 h1 fs-2 text-light" href="#" id="Brand">Bildit Hub</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand mb-0 h1 fs-1 text-secondary" href="#" id="Brand">Bildit Hub</a>
+                <button className="navbar-toggler btn-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link text-secondary" href="#">Home</a>
+                            <a className="nav-link" href="#">Home</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-secondary" href="#">Leaderboard</a>
+                            <a className="nav-link" href="#">Leaderboard</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link text-secondary" href="#">Roadmap</a>
+                            <a className="nav-link" href="#">Roadmap</a>
                         </li>
                     </ul>
                 </div>
